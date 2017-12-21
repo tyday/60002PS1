04972d028a066466466c7dd6c7d6d2ec422140ca
Ty Day <tyrday@gmail.com>
2017-12-21 14:39:23 -0500

    add: load_cows now loads cows

--- ps1a.py
+++ ps1a.py
@@ -25,7 +25,12 @@ def load_cows(filename):
     a dictionary of cow name (string), weight (int) pairs
     """
     # TODO: Your code here
-    pass
+    with open(filename) as f: #gets textfile with cowname,weight
+        read_data = f.readlines()
+        cow_dict = {}
+        for line in read_data:  #splits each line by comma sep. Adds to dictionary: cow_dict
+            cow_info = line.split(',')
+            cow_dict[cow_info[0]] = cow_info[1].strip()
 
 # Problem 2
 def greedy_cow_transport(cows,limit=10):
